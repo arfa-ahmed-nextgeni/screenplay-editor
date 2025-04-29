66f6a4680dcfbd11e0be405aa1d8bb964cd4c021
arfa ahmed <arfa.ahmed@nextgeni.com>
2025-04-29 16:06:56 +0500

    feat(git-hooks): configure husky and lint-staged for pre-commit hooks
    - Setup Husky and Lint-staged for pre-commit hooks.
    - Integrated ESLint plugins for:
      - Sorting imports.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,34 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
+
 import { FlatCompat } from "@eslint/eslintrc";
+import js from "@eslint/js";
 import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
 
+/** @type {import('eslint').Linter.Config[]} */
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    plugins: {
+      "simple-import-sort": simpleImportSort,
+    },
+    rules: {
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
+    },
+  },
   eslintPluginPrettierRecommended,
 ];
 
